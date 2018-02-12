29eaccc87ac3f2304bf85434ea928fb04231dee2
l0rraine <idn.lee@gmail.com>
2018-02-12 15:24:13 +0800

    改为vue

---
+++ src/resources/assets/js/components/ddvue/crud/breadcrumbs.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-row type="flex" class="row-bg" justify="space-between">
+        <el-col :xs="22" :sm="18" :md="10" :lg="12" :xl="12">
+            <h3 style="padding:0 20px;">
+                {{ title }}
+            </h3>
+        </el-col>
+        <el-col :span="4">
+            <el-breadcrumb separator-class="el-icon-arrow-right" v-for="(item,index) in items" :key="index">
+                <el-breadcrumb-item @click.native="handleClick(item.link)">首页</el-breadcrumb-item>
+            </el-breadcrumb>
+        </el-col>
+    </el-row>
+</template>
+<script>
+    export default {
+        name: 'DdvCrudBreadcrumbs',
+        props: {
+            items: Array,
+            title: String
+        },
+        methods: {
+            handleClick(link) {
+                console.log(link);
+                this.$emit('onClick',link);
+            }
+        }
+    }
+</script>
+<style>
+</style>
---
+++ src/resources/assets/js/components/ddvue/crud/form.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-form label-width="80px">
+        <slot name="fields"></slot>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit(url)">立即创建</el-button>
+            <el-button>取消</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+<script>
+    export default {
+        name: 'DdvCrudForm',
+        props: {
+            url: String,
+        },
+        methods: {
+            onSubmit(link) {
+                console.log(link);
+                this.$emit('onSubmit',link);
+            }
+        }
+    }
+</script>
